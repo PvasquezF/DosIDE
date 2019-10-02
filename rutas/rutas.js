9823823ca8f7d25db8015950b6f24c5a075ce50e
pvasquez <alexanderpavelv32@gmail.com>
2019-10-02 11:23:56 -0600

    Conexion NodeJs-Java
    Se realizo la conexion mediante sockets, ya se reciben mensajes desde java  y viceversa.

--- rutas/rutas.js
+++ rutas/rutas.js
@@ -1,5 +1,29 @@
-exports.routesConfig = function (app) {
+var net = require('net');
+exports.routesConfig = function (app, socket) {
     app.get("/", (req, res) => {
-        return res.render('index.ejs');
+        var client = net.connect(3002, 'localhost');
+        server = net.createServer(function (socket) {
+            console.log('Cliente java conectado');
+            socket.on('error', () => {
+                console.log("error");
+            });
+            //Aqui viene el texto de respuesta de java
+            socket.on('data', function (data) {
+                data = data.toString('utf-8');
+                server.close();
+                return res.render('index.ejs', {
+                    data: data
+                });
+            });
+        }).listen(3001, "localhost", () => {
+            console.log("Socket open in: " + 3001);
+        });
+        //Aqui debe ir el texto a enviar
+        client.write(`Hello from node.js
+        adfadsfas
+        fad
+        fsf`);
+        client.end();
+        //return res.render('index.ejs');
     });
 }
--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const bodyParser = require("body-parser");
 const express = require('express');
 const index = require('./rutas/rutas');
 const app = express();
+const net = require('net');
+let socketPort = 3001;
+let host = "localhost";
+var socket;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
-//app.use('/', express.static(process.cwd() + '/'));
+app.use('/', express.static(process.cwd() + '/'));
 app.use(express.static(__dirname + '/Web'));
-index.routesConfig(app);
+index.routesConfig(app, socket);
 app.set("view engine", "ejs");
+
 app.listen(3000, () => {
-	console.log(`Running on http://localhost:3000`);
+    console.log(`Running on http://localhost:3000`);
 });
